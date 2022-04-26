8ae7deb520815a88c5f4e9a3c9edfaa614f023a1
HTMLhead <mrdoo309@gmail.com>
2022-04-26 16:24:59 +0900

    :rocket: 11ty-blog로 output수정 후 배포

--- .eleventy.js
+++ .eleventy.js
@@ -13,7 +13,7 @@ module.exports = function (eleventyConfig) {
     dir: {
       input: "src",
       includes: "_includes",
-      output: "_site",
+      output: "11ty-blog",
     },
     templateFormats: ["md", "njk", "html"],
     markdownTemplateEngine: "njk",
